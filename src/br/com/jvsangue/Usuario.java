8d9850a88b2eda8a6eced97c1011d7311d13cfb4
Fernando <fernandohemmeldossantos@gmail.com>
2022-07-14 19:32:05 -0300

    Primeiro commit com a classes Usuario e MenuInicial

---
+++ src/br/com/jvsangue/Usuario.java
@@ -0,0 +1,45 @@
+package br.com.jvsangue;
+
+public class Usuario {
+	
+	private String nome;
+	private int idade;
+	private long cpf;
+	private String endereco;
+	private String regiao;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public long getCpf() {
+		return cpf;
+	}
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getRegiao() {
+		return regiao;
+	}
+	public void setRegiao(String regiao) {
+		this.regiao = regiao;
+	}
+	
+	
+
+
+}
