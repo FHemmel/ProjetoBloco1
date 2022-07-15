d93a7d32d4605e3a86e1ebb443b871442318356c
luizavramos <luizavramos30@gmail.com>
2022-07-15 14:37:46 -0300

    questionário

--- src/br/com/jvsangue/Questionario.java
+++ src/br/com/jvsangue/Questionario.java
@@ -8,6 +8,10 @@ public class Questionario {
 	private int intervalo;
 	private int continua;
 	private int gravidez;
+	private int periodo;
+	private int parto;
+	private int idade;
+
 
 
 	Scanner leia =  new Scanner(System.in);
@@ -34,44 +38,60 @@ public class Questionario {
 			System.out.println("Para homens o intervalo mínimo para realizar doação de sangue é de 2 meses. Infelizmente você não está apto a realizar a doação."
 					+ "Deseja continuar respondendo o questionário? \n 1- Sim 2- Não ");
 			continua = leia.nextInt();
-			if (continua == 1) {
-				questionarioPermanente();
+			if (continua == 2) {
+				//menuiniciar metodo
 			}
-			else {
-				//MenuInicial.menu(); Colocar método menu inicial.
-			}
+			questionarioPermanente();
 		}
 
 	}
 
 	public void questionarioFem() {
-	
+
 		System.out.println("Você está em período gestacional?  \\n 1- Sim 2- Não");
 		gravidez = leia.nextInt();
 		if (gravidez == 1) { 
 			System.out.println("Você não está apta a realizar a doação devido estar em período gestacional. Deseja continuar respondendo o questionário ?  \n 1- Sim 2- Não");
 			continua = leia.nextInt();
+			if (continua == 2) {
+				//metodo menu inicial
+			}
 		}
-		
-		
+		System.out.println("Você esteve em período gestacional nos últimos 12 meses? \\n 1- Sim 2- Não");
+		periodo = leia.nextInt();
+		if (periodo == 1) {
+			System.out.println("Caso tenha sido parto normal você deve aguardar 90 dias e 180 dias para cesariana e"
+					+ "\n caso esteja amamentando deve aguardar 12 meses após o parto para estar apta a realizar a doação, você se enquadra nestes requisitos?"
+					+ "\n  1- Sim 2- Não");
+			parto = leia.nextInt();		
+			if (parto == 1) {
+				System.out.println("Infelizmente você não está apta a realizar a doação no momento."
+						+ " \n Deseja continuar respondendo? 1 - Sim 2 - Não");
+				continua = leia.nextInt();	
+				if (continua == 2) {
+					//metodo menu iniciar}
+				}
+			}
+		}
+
 		System.out.println("Doou sangue nos últimos 3 meses? \n 1- Sim 2- Não ");
 		intervalo = leia.nextInt();
-		if (intervalo == 2) {
-			questionarioPermanente();
-		}
-		else {
+		if (intervalo == 1) {
 			System.out.println("Para mulheres o intervalo mínimo para realizar doação de sangue é de 3 meses. Infelizmente você não está apta a realizar a doação."
 					+ "Deseja continuar respondendo o questionário? \n 1- Sim 2- Não ");
 			continua = leia.nextInt();
-			if (continua == 1) {
-				questionarioPermanente();
+			if(continua == 2) {
+				//metodo menu iniciar
 			}
-			else {
-				//MenuInicial.menu(); Colocar método menu inicial.
+			questionarioPermanente();
+			}
+		}
 
-			}
-	
+
 		public void questionarioPermanente() {
+			System.out.println("Qual a sua idade? ");
+			idade = leia.nextInt();
+			
 
 		}
 	}
