2ccacfdf28e14d2f9cf76daf03694caa91c1fffc
luizavramos <luizavramos30@gmail.com>
2022-07-16 20:18:43 -0300

    finalizando questionário

--- src/br/com/jvsangue/Questionario.java
+++ src/br/com/jvsangue/Questionario.java
@@ -4,23 +4,17 @@ import java.util.Scanner;
 
 public class Questionario extends Cadastro{
 
-	private int genero;
-	private int intervalo;
-	private int continua;
-	private int gravidez;
-	private int periodo;
-	private int parto;
-	private int idade;
-	private int doouAnteriormente;
-	private double peso;
-	private int doencasPermanentes;
-	private int p1,p2,p3,p4,p5,p7,ret;
+private int genero, drogas, intervalo, continua, gravidez, periodo, parto, idade, doouAnteriormente, doencasPermanentes;
 	
+	private double peso;
+
+	private int p1,p2,p3,p4,p5,p7,ret;
+
 
 
 
 	Scanner leia =  new Scanner(System.in);
-	
+
 	Usuario usu = new Usuario();
 
 	public void realizarQuestionario() {
@@ -42,8 +36,8 @@ public class Questionario extends Cadastro{
 			questionarioPermanente();
 		}
 		else {
-			System.out.println("Para homens o intervalo mínimo para realizar doação de sangue é de 2 meses. Infelizmente você não está apto a realizar a doação."
-					+ "Deseja continuar respondendo o questionário? \n 1- Sim 2- Não ");
+			System.out.println("Para homens o intervalo mínimo para realizar doação de sangue é de 2 meses.\n Infelizmente você não está apto a realizar a doação."
+					+ "\nDeseja continuar respondendo o questionário? \n 1- Sim 2- Não ");
 			continua = leia.nextInt();
 			if (continua == 2) {
 
@@ -59,7 +53,8 @@ public class Questionario extends Cadastro{
 		System.out.println("Você está em período gestacional?  \n 1- Sim 2- Não");
 		gravidez = leia.nextInt();
 		if (gravidez == 1) { 
-			System.out.println("Você não está apta a realizar a doação devido estar em período gestacional. Deseja continuar respondendo o questionário ?  \n 1- Sim 2- Não");
+			System.out.println("Você não está apta a realizar a doação devido estar em período gestacional."
+					+ "\n Deseja continuar respondendo o questionário ?  \n 1- Sim 2- Não");
 			continua = leia.nextInt();
 			if (continua == 2) {
 
@@ -69,15 +64,17 @@ public class Questionario extends Cadastro{
 		System.out.println("Você esteve em período gestacional nos últimos 12 meses? \n 1- Sim 2- Não");
 		periodo = leia.nextInt();
 		if (periodo == 1) {
-			System.out.println("Caso tenha sido parto normal você deve aguardar 90 dias e 180 dias para cesariana e"
-					+ "\n caso esteja amamentando deve aguardar 12 meses após o parto para estar apta a realizar a doação, você se enquadra nestes requisitos?"
+			System.out.println("Caso você tenha tido parto normal deve aguardar 90 dias \n"
+					+ "caso voê tenha tido parto cesariana deve aguardar 180 dias"
+					+ "\n caso esteja amamentando deve aguardar 12 meses após o parto para estar apta a realizar a doação. "
+					+ "\n você se enquadra nestes requisitos?"
 					+ "\n  1- Sim 2- Não");
 			parto = leia.nextInt();		
 			if (parto == 1) {
-				System.out.println("Infelizmente você n�o está apta a realizar a doação no momento."
+				System.out.println("Infelizmente você não está apta a realizar a doação no momento."
 						+ " \n Deseja continuar respondendo? 1 - Sim 2 - Não");
 				continua = leia.nextInt();	
-					if (continua == 2) {
+				if (continua == 2) {
 
 					MenuInicial.menu();
 				}
@@ -87,13 +84,14 @@ public class Questionario extends Cadastro{
 		System.out.println("Doou sangue nos últimos 3 meses? \n 1- Sim 2- Não ");
 		intervalo = leia.nextInt();
 		if (intervalo == 1) {
-			System.out.println("Para mulheres o intervalo mínimo para realizar doação de sangue é de 3 meses. Infelizmente você não está apta a realizar a doação."
-					+ "Deseja continuar respondendo o questionário? \n 1- Sim 2- Não ");
+			System.out.println("Para mulheres o intervalo mínimo para realizar doação de sangue são de 3 meses. "
+					+ "\nInfelizmente você não está apta a realizar a doação."
+					+ "\nDeseja continuar respondendo o questionário? \n 1- Sim 2- Não ");
 			continua = leia.nextInt();
 			if(continua == 2) {
 
 				MenuInicial.menu();
-				
+
 			}
 		}
 		questionarioPermanente();
@@ -103,36 +101,50 @@ public class Questionario extends Cadastro{
 	public void questionarioPermanente() {
 		System.out.println("Qual a sua idade? ");
 		idade = leia.nextInt();
-		if (idade < 16 && idade > 69 ) {
-			System.out.println("A faixa de idade apta a realizar a doação é de 16 a 69 anos. Infelizmente você não está apte a realizar a doação.");
-
+		if (idade < 16 || idade > 69 ){
+			System.out.println("A faixa de idade apta a realizar a doaÃ§Ã£o Ã© de 16 a 69 anos. \nInfelizmente você não está apte a realizar a doação.");
 			MenuInicial.menu();
 		}
-		if (idade >= 60 && idade <= 69) {
+		else if (idade>=60 && idade<=69) {
 			System.out.println("Você já doou sangue anteriormente? \n 1 - Sim 2 - Não");	
 			doouAnteriormente = leia.nextInt();
 			if(doouAnteriormente == 2) {
-				System.out.println("Infelizmente você não esté apte a realizar a doação. ");
-				
+				System.out.println("Infelizmente você não está apte a realizar a doação. ");
 				MenuInicial.menu();
 			}
 		}
 		System.out.println("Qual o seu peso? ");
 		peso = leia.nextDouble();
-		if (peso < 50) {
-			System.out.println("Para realizar a doação precisa ter 50kg ou mais. Infelizmente você não está apte a realizar a doação.");
-			
+		if (peso < 50.0) {
+			System.out.println("Para realizar a doação precisa ter 50kg ou mais. "
+					+ "\nInfelizmente você não está apte a realizar a doação.");
+
 			MenuInicial.menu();
 		}
-		System.out.println("Você teve hepatite após os 11 anos ou evidência das seguintes doenças hepatite B e C, AIDS doenças associadas ao vírus HTLV I e II e Doença de chagas?"
-				+ "\n Fez uso de drogas ilícitas injetáveis? \n Teve malária? \n Tem piercing em cavidade oral ou região genital?"
-				+ "\n Caso afirmativo para qualquer uma das perguntas acima digite 1. ");
+		System.out.println("Você teve alguma dessas doenças listadas abaixo:"
+				+ "\n Hepatite após os 11 anos "
+				+ "\n Evidência de hepatite B e C"
+				+ "\n AIDS  ou doenças associadas ao vírus HTLV I e II "
+				+ "\n Doença de chagas"
+				+  "\n Malária"
+				+ "\n Caso afirmativo para qualquer uma das perguntas acima digite 1, caso não digite 2. ");
 		doencasPermanentes = leia.nextInt();		
 		if (doencasPermanentes == 1) {
+			System.out.println("Infelizmente vocÃª nÃ£o poderÃ¡ realizar a doação de sangue.");
+			MenuInicial.menu();
+		}
+		System.out.println(" Você já fez uso de drogas ilícitas injetáveis? ou"
+				+ " \n Tem piercing em cavidade oral ou região genital?"
+				+ "\n Caso afirmativo para qualquer uma das perguntas acima digite 1, caso não digite 2.");
+		drogas = leia.nextInt();
+		if (drogas == 1) {
 			System.out.println("Infelizmente você não poderá realizar a doação de sangue.");
+			MenuInicial.menu();
 		}
 		questionarioTemporario();
 	}
+
+	
 	public void questionarioTemporario() {
 		p1();
 		p2();
