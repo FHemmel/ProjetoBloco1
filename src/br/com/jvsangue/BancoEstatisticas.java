55cfe03f49cc6bc15665d18b0eab712bb891c346
Amanda Falcão <amandaf.dias96@gmail.com>
2022-07-16 14:38:40 -0300

    Criando métodos para mostrar estatísticas

--- src/br/com/jvsangue/BancoEstatisticas.java
+++ src/br/com/jvsangue/BancoEstatisticas.java
@@ -1,6 +1,7 @@
 package br.com.jvsangue;
 
 public class BancoEstatisticas {
+	
 
 	public static void mostraEstGerais() {
 		System.out.println("========================================================");
@@ -31,6 +32,59 @@ public class BancoEstatisticas {
 						+ "\nentão compartilhe este programa para o máximo de pessoas que puder!");
 	}
 	
+	public static void mostraAPositivo() {
+		System.out.println("Você pode doar para pessoas com sangue AB+ e A+, e pode receber sangue "
+				+ "\nde pessoas A+, A-, O+ e O-. De acordo com o hemocentro Pró-Sangue de São Paulo, "
+				+ "\no estoque de sangue A+ está ESTÁVEL no momento, ufa! Mas não significa que ficará "
+				+ "\nassim para sempre, então responda nosso questionário para saber se você pode ser doador.");
+	}
+	
+	
+	public static void mostraANegativo() {
+		System.out.println("Você pode doar para pessoas com sangue A+, A-, AB+ e AB-, e pode receber sangue "
+				+ "\nde pessoas A- e O-. De acordo com o hemocentro Pró-Sangue de São Paulo, o estoque de sangue "
+				+ "\nA- está EM ALERTA, então que tal responder o questionário para saber se você pode doar?");
+	}
+	
+	public static void mostraBPositivo() {
+		System.out.println("Você pode doar para pessoas com sangue B+ e AB+, e pode receber sangue de pessoas "
+				+ "\nB+, B-, O+ e O-. De acordo com o hemocentro Pró-Sangue de São Paulo, o estoque de sangue B+ "
+				+ "\nestá ESTÁVEL no momento, ufa! Mas não significa que ficará assim para sempre, então responda "
+				+ "\nnosso questionário para saber se você pode ser doador.");
+	}
+	
+	public static void mostraBNegativo() {
+		System.out.println("Você pode doar para pessoas com sangue B+, B-, AB+ e AB-, e pode receber de pessoas "
+				+ "\nB- e O-. De acordo com o hemocentro Pró-Sangue de São Paulo, o estoque de sangue B- está "
+				+ "\nEM ESTADO CRÍTICO, portanto vamos agilizar seu questionário para que você possa saber se pode doar!");
+	}
+	
+	public static void mostraABPositivo() {
+		System.out.println("Você só pode doar para pessoas com sangue AB+, mas pode receber de todos os outros "
+				+ "\ntipos sanguíneos, por isso é conhecido como receptor universal. De acordo com o hemocentro "
+				+ "\nPró-Sangue de São Paulo, o estoque de sangue AB+ está ESTÁVEL no momento, ufa! Mas não significa "
+				+ "\nque ficará assim para sempre, então responda nosso questionário para saber se você pode ser doador.");
+	}
+	
+	public static void mostraABNegativo() {
+		System.out.println("Você pode doar para pessoas com sangue AB+ e AB-, e pode receber de pessoas "
+				+ "\nA-, B-, O- e AB-. De acordo com o hemocentro Pró-Sangue de São Paulo, o estoque de sangue AB- está "
+				+ "\nESTÁVEL no momento, ufa! Mas não significa que ficará assim para sempre, então responda nosso "
+				+ "\nquestionário para saber se você pode ser doador.");
+	}
+	
+	public static void mostraOPositivo() {
+		System.out.println("Você pode doar para pessoas com sangue A+, B+, O+ e AB+, e pode receber de pessoas O+ e O-. "
+				+ "\nDe acordo com o hemocentro Pró-Sangue de São Paulo, o estoque de sangue O+ está EM ESTADO CRÍTICO, "
+				+ "\nportanto vamos agilizar seu questionário para que você possa saber se pode doar!");
+	}
+	
+	public static void mostraONegativo() {
+		System.out.println("Você pode doar para pessoas com todos os tipos sanguíneos e é conhecido como doador universal,"
+				+ "\nmas só pode receber de pessoas com sangue O-. De acordo com o hemocentro Pró-Sangue de São Paulo, o "
+				+ "\nestoque de sangue O- está EM ESTADO CRÍTICO, portanto vamos agilizar seu questionário para que você "
+				+ "\npossa saber se pode doar!");
+	}
 	
 
 }
--- src/br/com/jvsangue/Estatisticas.java
+++ src/br/com/jvsangue/Estatisticas.java
@@ -8,8 +8,72 @@ public class Estatisticas extends BancoEstatisticas {
 		
 		Scanner input = new Scanner(System.in);
 		
+		// getName de Cadastro
+		
 		mostraEstGerais();
 		
+		System.out.println();
+		System.out.println("Agora vamos ver alguns dados sobre o seu sangue."
+						+ "\nPrimeiro, me diga qual seu tipo sanguíneo: ");
+		
+		System.out.println("(1) A+");
+		System.out.println("(2) A-");
+		System.out.println("(3) B+");
+		System.out.println("(4) B-");
+		System.out.println("(5) AB+");
+		System.out.println("(6) AB-");
+		System.out.println("(7) O+");
+		System.out.println("(8) O-");
+		System.out.println("(9) Não sei");
+		char opcao1 = input.next().charAt(0);
+		
+		switch(opcao1) {
+		case '1':
+			mostraAPositivo();
+			break;
+		case '2':
+			mostraANegativo();
+			break;
+		case '3':
+			mostraBPositivo();
+			break;
+		case '4':
+			mostraBNegativo();
+			break;
+		case '5':
+			mostraABPositivo();
+			break;
+		case '6':
+			mostraABNegativo();
+			break;
+		case '7':
+			mostraOPositivo();
+			break;
+		case '8':
+			mostraONegativo();
+			break;
+		case '9':
+			System.out.println("Veja como você pode descobrir seu tipo sanguíneo de duas formas diferentes:"
+					+ "\n1) Faça um exame de sangue comum e peça para descobrir seu tipo sanguíneo;"
+					+ "\n2) Responda nosso questionário para descobrir se você está apto a dor sangue,"
+					+ "\npois ao doar você ficará sabendo qual seu tipo e ainda ganha um cartão de doador!");
+		}
+		
+		System.out.println();
+		System.out.println("Deseja retornar ao menu principal?");
+		System.out.println("(1) Sim");
+		System.out.println("(2) Não");
+		char opcao2 = input.next().charAt(0);
+		
+		switch (opcao2) {
+		case '1':
+			// retorna ao menu inicial
+			break;
+		case '2':
+			System.out.println("Você saiu do programa.");
+			System.exit(0);
+			break;
+		}
 		input.close();
 	}
 }
